d98ca7b87c23cb0eb84f1198e801714801babb20
Luner <jluner@usc.edu>
2017-10-17 16:36:59 -0700

    Adds StringMessage Class
    A Message class only containing a string with either information or a message to display

---
+++ CSCI201L_FinalProject_MessagesServer/src/objects/StringMessage.java
@@ -0,0 +1,13 @@
+package objects;
+
+public class StringMessage {
+	private String message;
+	
+	public StringMessage(String message) {
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		return this.message;
+	}
+}
