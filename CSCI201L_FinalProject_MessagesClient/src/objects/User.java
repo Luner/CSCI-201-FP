a43f36246ead7c27ea119dc28f20f7b1eef92cff
Luner <jluner@usc.edu>
2017-10-17 11:57:58 -0700

    Added User in Client
    Added the User object in the client so that the client knowns which user is currently active

---
+++ CSCI201L_FinalProject_MessagesClient/src/objects/User.java
@@ -0,0 +1,45 @@
+package objects;
+
+public class User {
+	private String username;
+	private String password;
+	private int uid; //user id 
+	
+	public User(String username, String password, int uid) {
+		this.username = username;
+		this.password = password;
+		this.uid = uid;
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public int getUid() {
+		return this.uid;
+	}
+	
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o == this) {
+	        return true;
+	    }
+	    if (!(o instanceof User)) {
+	    		return false;
+	    }
+	      
+	    User user = (User) o;
+	    
+	    if(this.username.equals(user.getUsername()) && this.password.equals(user.getPassword())
+	    		&& this.uid == user.getUid()) {
+	    		return true;
+	    }
+	      
+	    return false;
+	}
+}
