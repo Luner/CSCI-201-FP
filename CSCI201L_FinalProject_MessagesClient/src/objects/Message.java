aa5fd8e9f93c1ad795777abb1974692b6c58cb10
Luner <jluner@usc.edu>
2017-10-17 16:39:02 -0700

    Modifies Message
    makes the Messaging abstract class public for other classes to get 'instances' of

--- CSCI201L_FinalProject_MessagesClient/src/objects/Message.java
+++ CSCI201L_FinalProject_MessagesClient/src/objects/Message.java
@@ -2,7 +2,6 @@ package objects;
 
 import java.io.Serializable;
 
-abstract class Message implements Serializable{
+public abstract class Message implements Serializable{
 	private static final long serialVersionUID = 1L;
-	int type;
 }
