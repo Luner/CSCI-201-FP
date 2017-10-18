1a7179223087ad4ab8d698a2811106139ee0c29d
Luner <jluner@usc.edu>
2017-10-17 17:40:05 -0700

    Comments in Server.VerificationMessage
    Added usefil comments in the server.verificationMessage class

--- CSCI201L_FinalProject_MessagesServer/src/objects/VerificationMessage.java
+++ CSCI201L_FinalProject_MessagesServer/src/objects/VerificationMessage.java
@@ -1,21 +1,26 @@
 package objects;
 
+//A message meant to be sent to the server for verification
 public class VerificationMessage extends Message{
 	private static final long serialVersionUID = 1L;
 	
+	//The message contains a userneme and password to be compared to find the
+	//respective user if it exists
 	private String username;
 	private String password;
 	
-
+	//Constructor: sets all of the variable
 	public VerificationMessage(String username, String password) {
 		this.username = username;
 		this.password = password;
 	}
 	
+	//getter for username
 	public String getUsername() {
 		return username;
 	}
 	
+	//Getter for password
 	public String getPassword() {
 		return password;
 	}
