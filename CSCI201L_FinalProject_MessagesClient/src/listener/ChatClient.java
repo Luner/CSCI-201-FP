c6867db8f9ac32354be7562972a61011dcd13f61
Jonathan Luner <jluner@usc.edu>
2017-10-18 22:23:08 -0700

    Modified ClientChat
    Created a "Better Looking" (not that great) application login Window

--- CSCI201L_FinalProject_MessagesClient/src/listener/ChatClient.java
+++ CSCI201L_FinalProject_MessagesClient/src/listener/ChatClient.java
@@ -1,24 +1,30 @@
 package listener;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.net.MalformedURLException;
 import java.net.Socket;
+import java.util.ArrayList;
 
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.StrokeType;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import objects.message.ChatMessage;
 import objects.message.Message;
@@ -28,17 +34,211 @@ import objects.message.VerificationResponseMessage;
 
 public class ChatClient extends Application {
 	
-	//GRAPHICAL USER INTERFACE
-	Button login;
-	Button sendMessage;
+//GRAPHICAL USER INTERFACE ------------------
+	
+	//LogInPage
+	Scene loginScene;
+	VBox loginLayout;
+	
+	//Menu
+	MenuBar menu;
+	Menu userMenu;
+	MenuItem logoutMenuItem;
+	
+	//Pane
+	AnchorPane loginPane;
+	
+	//Labels
+	Label usernameLabel;
+	Label passwordLabel;
+	
+	//TextFields
 	TextField username;
 	TextField password;
-	TextField chatText;
-	Text usernameLabel;
-	Text passwordLabel;
-	Scene chat;
-	Stage window;
-	VBox chatLayout;
+	
+	//Login Button
+	Button login;
+	
+	//Decoration
+	ArrayList<Circle> circles;
+//-------------------------------------------
+	
+	
+	//Button login;
+	//Button sendMessage;
+	//TextField chatText;
+	//Scene chat;
+	//Stage window;
+	//VBox chatLayout;
+	
+	public void initializeLoginPage() {
+		//layout
+		loginLayout = new VBox();
+		loginLayout.setPrefHeight(400.0);
+		loginLayout.setPrefWidth(640.0);
+		
+		//MenuBar
+		menu = new MenuBar();
+		VBox.setVgrow(menu, Priority.NEVER);
+		
+		//Menu
+		userMenu = new Menu();
+		userMenu.setMnemonicParsing(false);
+		userMenu.setText("User");
+		
+		//MenuItems 
+		logoutMenuItem = new MenuItem();
+		logoutMenuItem.setText("Logout");
+		
+		//add UserMenu to MenuBar
+		menu.getMenus().add(userMenu);
+		//Add logoutMenuItem to userMenu
+		userMenu.getItems().add(logoutMenuItem);
+		
+		//Pane
+		loginPane = new AnchorPane();
+		loginPane.setMaxHeight(-1.0);
+		loginPane.setMaxWidth(-1.0);
+		loginPane.setPrefHeight(-1.0);
+		loginPane.setPrefWidth(-1.0);
+		loginPane.setStyle("-fx-border-color: black; -fx-background-color: grey;");
+		VBox.setVgrow(loginPane, Priority.ALWAYS);
+		
+		//Font
+		
+		
+		//UsernameLabel
+		usernameLabel = new Label();
+		usernameLabel.setLayoutX(147.0);
+		usernameLabel.setLayoutY(79.0);
+		usernameLabel.setPrefHeight(53);
+		usernameLabel.setPrefWidth(140);
+		usernameLabel.setText("Username:");
+		usernameLabel.setFont(Font.font(30));
+		
+		//UsernameLabel
+		passwordLabel = new Label();
+		passwordLabel.setLayoutX(154.0);
+		passwordLabel.setLayoutY(132.0);
+		passwordLabel.setPrefHeight(45);
+		passwordLabel.setPrefWidth(133);
+		passwordLabel.setText("Password:");
+		passwordLabel.setFont(Font.font(30));
+		
+		//UsernameTextField
+		username = new TextField();
+		username.setLayoutX(293.0);
+		username.setLayoutY(94.0);
+		username.setPrefHeight(26);
+		username.setPrefWidth(200);
+		
+		//PasswordTextField
+		password = new TextField();
+		password.setLayoutX(293.0);
+		password.setLayoutY(142.0);
+		password.setPrefHeight(26);
+		password.setPrefWidth(200);
+		
+		//LoginButton
+		login = new Button();
+		login.setLayoutX(278.0);
+		login.setLayoutY(202.0);
+		login.setMnemonicParsing(false);
+		login.setPrefWidth(62.0);
+		login.setStyle("-fx-background-color: #59f429;");
+		login.setText("LogIn");
+		
+		
+		//Circles
+		circles = new ArrayList<Circle>();
+		
+		for(int i = 0; i < 12; i++) {
+			Circle c = new Circle();
+			c.setFill(Color.web("#59f429"));
+			c.setStroke(Color.BLACK);
+			c.setStrokeType(StrokeType.INSIDE);
+			circles.add(c);
+		}
+		
+		Circle temp = circles.get(0);
+		temp.setLayoutX(79.0);
+		temp.setLayoutY(303.0);
+		temp.setRadius(41);
+		
+		temp = circles.get(1);
+		temp.setLayoutX(190.0);
+		temp.setLayoutY(263.0);
+		temp.setRadius(27);
+	
+		temp = circles.get(2);
+		temp.setLayoutX(96.0);
+		temp.setLayoutY(211.0);
+		temp.setRadius(20);
+		
+		temp = circles.get(3);
+		temp.setLayoutX(81.0);
+		temp.setLayoutY(76.0);
+		temp.setRadius(36);
+		
+		temp = circles.get(4);
+		temp.setLayoutX(309.0);
+		temp.setLayoutY(314.0);
+		temp.setRadius(36);
+		
+		temp = circles.get(5);
+		temp.setLayoutX(415.0);
+		temp.setLayoutY(240.0);
+		temp.setRadius(22);
+		
+		temp = circles.get(6);
+		temp.setLayoutX(539.0);
+		temp.setLayoutY(290.0);
+		temp.setRadius(60);
+		
+		temp = circles.get(7);
+		temp.setLayoutX(586.0);
+		temp.setLayoutY(132.0);
+		temp.setRadius(31);
+		
+		temp = circles.get(8);
+		temp.setLayoutX(519.0);
+		temp.setLayoutY(57.0);
+		temp.setRadius(22);
+		
+		temp = circles.get(9);
+		temp.setLayoutX(420.0);
+		temp.setLayoutY(47.0);
+		temp.setRadius(27);
+		
+		temp = circles.get(10);
+		temp.setLayoutX(293.0);
+		temp.setLayoutY(59.0);
+		temp.setRadius(16);
+		
+		temp = circles.get(11);
+		temp.setLayoutX(207.0);
+		temp.setLayoutY(47.0);
+		temp.setRadius(22);
+		
+		
+		
+		//Add Children to Pane
+		loginPane.getChildren().add(usernameLabel);
+		loginPane.getChildren().add(passwordLabel);
+		loginPane.getChildren().add(username);
+		loginPane.getChildren().add(password);
+		loginPane.getChildren().add(login);
+		for(Circle circle : circles){
+			loginPane.getChildren().add(circle);
+		}
+		
+		//Add Children to Layout
+		loginLayout.getChildren().add(menu);
+		loginLayout.getChildren().add(loginPane);
+		
+
+		loginScene = new Scene(loginLayout);
+	}
 	
 	//SERVER CLIENT COMMUNICATION
 	private ObjectInputStream ois;
@@ -53,59 +253,43 @@ public class ChatClient extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("Messaging Application");
-
-	
-		usernameLabel = new Text("username: ");
-		passwordLabel = new Text("password: ");	
+		initializeLoginPage();
 		
-		username = new TextField();
-		password = new TextField();
-		chatText = new TextField();
-		
-		login = new Button();
-		login.setText("Login");
+		//Set what happens on button press
 		login.setOnAction(new EventHandler<ActionEvent>(){
 
 			@Override
 			public void handle(ActionEvent event) {
+				System.out.println("Pressed");
 				if(login(username.getText(), password.getText())) {
-					primaryStage.setScene(chat);
-					Thread th = new Thread(task);
-					th.setDaemon(true);
-					th.start();
+					System.out.println("Logged In");
+					//primaryStage.setScene(chat);
+					//Thread th = new Thread(task);
+					//th.setDaemon(true);
+					//th.start();
 				} 				
 			}
 		});
 		
-		sendMessage = new Button();
-		sendMessage.setText("Send Message");
-		sendMessage.setOnAction(e -> {
-			send(chatText.getText());
-			chatText.setText("");
-		});
+		primaryStage.setTitle("Messaging Application");
+		
+		
+		//sendMessage = new Button();
+		//sendMessage.setText("Send Message");
+		//sendMessage.setOnAction(e -> {
+		//	send(chatText.getText());
+		//	chatText.setText("");
+		//});
 		
 
-		chatLayout = new VBox();
-		HBox layout = new HBox();
-		
-		layout.setPadding(new Insets(15, 12, 15, 12));
-		layout.setSpacing(10);
-	    
-		layout.setStyle("-fx-background-color: #336699;");
-		layout.getChildren().add(usernameLabel);
-		layout.getChildren().add(username);
-		layout.getChildren().add(passwordLabel);
-		layout.getChildren().add(password);
-		layout.getChildren().add(login);
-		chatLayout.getChildren().add(chatText);	
-		chatLayout.getChildren().add(sendMessage);
-		Scene scene = new Scene(layout, 600, 400);
-		chat = new Scene(chatLayout, 600, 400);
-		primaryStage.setScene(scene);
+		//chatLayout = new VBox();
+		//chatLayout.getChildren().add(chatText);	
+		//chatLayout.getChildren().add(sendMessage);
+		//chat = new Scene(chatLayout, 600, 400);
+		primaryStage.setScene(loginScene);
 		primaryStage.show();
 
-		setUpChatClient("localhost", 6789);	
+		//setUpChatClient("localhost", 6789);	
 	}
 	
 	private void setUpChatClient(String hostname, int port) {
@@ -220,7 +404,7 @@ public class ChatClient extends Application {
         					Platform.runLater(new Runnable() {
         		    	        @Override
         		    	        public void run() {
-        		    	        	chatLayout.getChildren().add(new Text(((StringMessage) message).getMessage()));
+        		    	        	//chatLayout.getChildren().add(new Text(((StringMessage) message).getMessage()));
         		    	        }
         		    	      });
         					
