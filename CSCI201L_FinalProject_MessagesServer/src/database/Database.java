83b73cddf39be58e71c8511f637983ea8980c890
Christopher Turner <christo.turner@gmail.com>
2017-10-22 17:01:45 -0700

    Added login method, included SQL DLL

--- CSCI201L_FinalProject_MessagesServer/src/database/Database.java
+++ CSCI201L_FinalProject_MessagesServer/src/database/Database.java
@@ -7,12 +7,16 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Database {
+	private String hostname;
+	private int port;
 	private String username;
 	private String password;
 	private String database;
 	private Connection conn;
 
-	public Database(String username, String password, String database) {
+	public Database(String hostname, int port, String username, String password, String database) {
+		this.hostname = hostname;
+		this.port = port;
 		this.username = username;
 		this.password = password;
 		this.database = database;
@@ -20,8 +24,8 @@ public class Database {
 
 	public boolean connect() {
 		try {
-			conn = DriverManager.getConnection(
-					"jdbc:mysql://localhost:3306/" + database + "?user=" + username + "&password=" + password);
+			conn = DriverManager.getConnection("jdbc:mysql://" + hostname + ":" + port + "/" + database + "?user="
+					+ username + "&password=" + password);
 			return true;
 		} catch (SQLException sqle) {
 			System.out.println("Unable to connect to database with specified paramters.");
@@ -30,7 +34,7 @@ public class Database {
 	}
 
 	public void registerUser(String username, String password) {
-		String insertQuery = "INSERT users SET username = ? , password = ?";
+		String insertQuery = "INSERT users SET Username = ? , Password = ?";
 		try (PreparedStatement ps = conn.prepareStatement(insertQuery)) {
 			ps.setString(1, username);
 			ps.setString(2, password);
@@ -40,7 +44,17 @@ public class Database {
 		}
 	}
 
-	public void loginUser(String uername, String password) {
-		
+	public int loginUser(String username, String password) {
+		String selectQuery = "SELECT ID FROM users WHERE Username = ? , Password = ?";
+		try (PreparedStatement ps = conn.prepareStatement(selectQuery)) {
+			ps.setString(1, username);
+			ps.setString(2, password);
+			ResultSet rs = ps.executeQuery();
+			rs.next();
+			return rs.getInt(1);
+		} catch (SQLException sqle) {
+			System.out.println("Failed to create user with username: " + username);
+			return -1;
+		}
 	}
 }
