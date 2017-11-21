2528b8c25bef9192c91ffc37710c50482080f598
Luner <jluner@usc.edu>
2017-11-20 18:40:29 -0800

    added array

--- CSCI201L_FinalProject_MessagesServer/src/objects/message/ContactsMessage.java
+++ CSCI201L_FinalProject_MessagesServer/src/objects/message/ContactsMessage.java
@@ -5,21 +5,25 @@ import java.util.ArrayList;
 
 public class ContactsMessage implements Serializable {
 	private static final long serialVersionUID = 1L;
-	private ArrayList<String> contacts;
+	private String[] contacts;
 
 	public ContactsMessage(ArrayList<String> contacts) {
-		this.contacts = contacts;
+		this.contacts = new String[contacts.size()];
+		for(int i = 0; i < contacts.size(); i++) {
+			this.contacts[i] = contacts.get(i);
+		}
+		
 	}
 
 	public ArrayList<String> getContacts() {
-		if (contacts.contains("Guest")) {
-			contacts.remove("Guest");
+		ArrayList<String> result = new ArrayList<String>();
+		for(int i = 0; i < this.contacts.length; i++) {
+			if(contacts[i].equals("Guest") || contacts[i].equals("Bot")) {
+				//Do Nothing
+			} else {
+				result.add(contacts[i]);
+			}
 		}
-
-		if (contacts.contains("Bot")) {
-			contacts.remove("Bot");
-		}
-		return contacts;
-
+		return result;
 	}
 }
